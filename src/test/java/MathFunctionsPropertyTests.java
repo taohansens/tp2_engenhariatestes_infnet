a0acd7c133da63c918b6c79eba862cd09dcf986e
taohansens <taohansens@gmail.com>
2025-03-18 22:03:28 -0300

    MathFunctions b) e c)

--- src/test/java/MathFunctionsPropertyTests.java
+++ src/test/java/MathFunctionsPropertyTests.java
@@ -1,8 +1,10 @@
 
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
+import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+
+import java.util.List;
 
 public class MathFunctionsPropertyTests {
     @Property
@@ -10,5 +12,25 @@ public class MathFunctionsPropertyTests {
         int result = MathFunctions.multiplyByTwo(number);
         Assertions.assertTrue(result >= number,
                 () -> "O resultado " + result + " não é maior ou igual a " + number);
+    }
+
+    @Provide("positiveNumbers")
+    public Arbitrary<Integer> positiveNumbers() {
+        return Arbitraries.integers().between(1, 1073741823);
+    }
+
+    @Property
+    void multiplyByTwoShouldBeGreaterOrEqualWithSafeData(@ForAll("positiveNumbers") int number) {
+        int result = MathFunctions.multiplyByTwo(number);
+        Assertions.assertTrue(result >= number,
+                () -> "Erro: O resultado " + result + " não é maior ou igual a " + number);
+    }
+
+    @Property(tries = 90000, edgeCases = EdgeCasesMode.FIRST)
+    void multiplyByTwoShouldReturnEvenForEvenInput(@ForAll("positiveNumbers") int number) {
+        Assumptions.assumeTrue(number % 2 == 0);
+        int result = MathFunctions.multiplyByTwo(number);
+        Assertions.assertTrue(result % 2 == 0,
+                () -> "Erro: O resultado " + result + " não é par para o número " + number);
     }
 }
